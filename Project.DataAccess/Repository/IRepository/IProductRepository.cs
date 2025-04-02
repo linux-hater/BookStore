4425dadbb508424be159f7d2b2c49d6b6f7b80dd
linux-hater <Jeremy.Ilkiv@gmail.com>
2025-04-02 22:59:23 +0300

    feat/ new repository for product

---
+++ Project.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP_NET_core_Project.Models;
+
+namespace Project.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepo<Product>
+    {
+        void Update(Category obj);
+    }
+}
