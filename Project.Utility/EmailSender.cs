89f00213601532a0232471dd777804f8f51549f2
linux-hater <Jeremy.Ilkiv@gmail.com>
2025-04-27 14:38:36 +0300

    feat Role creating

---
+++ Project.Utility/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Project.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email
+            //later needed to implement normall end point
+            return Task.CompletedTask;
+        }
+    }
+}
